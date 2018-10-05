70618ff370d4f55e5e8f76e0ede0c6440fdda7cd
Jason Vance <=>
2018-10-05 13:15:23 -0500

    add recipes fake data and styling for recipe-box

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-//let recipes = {};
+let props = {
+    loading: false
+}
 
-$(document).ready(function () {
+$(document).ready(function () {    
     getRecipes()
-        .then(recipes =>  displayRecipes(recipes));
+        .then(recipes => {
+            displayRecipes(recipes)
+            hideLoading();
+        });
 })
 
 function getRecipeHtml(recipe) {
     const html = `
         <div class="col-4">
             <div class="recipe-box">
-                <a href="${recipe.url}" target="_blank"><img src="${recipe.image}"></a>
+                <a href="${recipe.url}" target="_blank"><img class="card__picture" src="${recipe.image}"></a>
                 <div class="recipe-text">
-                    <span class="name">${recipe.label}</span>
+                    <span class="recipe-text__name">${recipe.label}</span>
                     <span> ${recipe.totalTime} minutes</span>
                 </div>
             </div>
@@ -26,9 +31,9 @@ function displayRecipes(recipes) {
     for (let i = 0; i < tmpArray.length; i++) {
         const recipeHtml = getRecipeHtml(tmpArray[i].recipe);
         if ((i + 1) % 3 === 1) {
-            html += `<div class="row">${recipeHtml}`;
+            html += `<div class="row">${recipeHtml}`; // open the row div
         } else if ((i + 1) % 3 === 0 || i === (tmpArray.length - 1)) {
-            html += `${recipeHtml}</div>`;
+            html += `${recipeHtml}</div>`; // end the row div
         } else {
             html += recipeHtml;
         }
@@ -36,20 +41,30 @@ function displayRecipes(recipes) {
     $('.js-recipes').replaceWith(html);
 }
 
+function showLoading() {
+    $('.js-loading').show();
+    $('.js-recipes').hide();
+}
+
+function hideLoading() {
+    $('.js-loading').hide();
+    $('.js-recipes').show();
+}
+
 function getRecipes() {
+    // set as loading
+   showLoading();
     return new Promise((resolve, reject) => {
         const url = `https://api.edamam.com/search?q=grilled cheese&app_id=cb67d7d3&app_key=0fcf8a75711dcde2d2e9af5d36837b9e&from=0&to=50&time=1-30`
         getJson(url)
             .then(res => {
                 resolve(res)
-                //recipes = res;           
             })
             .catch(err => {
                 console.log("ERROR", err);
             })
     })
 }
-
 
 function getJson(url) {
     return new Promise((resolve, reject) => {
